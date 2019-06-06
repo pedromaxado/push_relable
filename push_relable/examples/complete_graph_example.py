0019e74d3027142f0faccef09221c20129e85e95
Pedro Otávio <p.o.maxado@gmail.com>
2019-06-06 14:59:12 -0300

    more tests

--- push_relable/examples/complete_graph_example.py
+++ push_relable/examples/complete_graph_example.py
@@ -1,21 +1,48 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
+from timeit import default_timer as timer
 from push_relable.classes import Graph
 from push_relable.algorithms import push_relabel
 
 
-def complete_graph_example(n):
+def complete_graph_example(n, s, t):
     g = Graph(size=n)
 
     for u in range(n):
         for v in range(n):
             if u != v:
-                g.add_edge(u, v, cap=np.random.randint(1, 10))
+                g.add_edge(u, v, cap=1)
 
-    max_flow = push_relabel(g, s=0, t=9)
+    print("finished building graph")
+
+    start = timer()
+    max_flow = push_relabel(g, s=s, t=t)
+    duration = timer() - start
 
     print(f"Maximum flow: {max_flow}")
+    print()
+
+    return duration
+
+
+def run_tests():
+
+    durations = []
+
+    for n in [i*10 for i in range(1, 40)]:
+        duration = complete_graph_example(n, 0, 1)
+        durations.append(duration)
+        print(n, duration)
+
+    print(durations)
+
+    x = np.log(np.array([i*10 for i in range(1, 40)]))
+    y = np.log(np.array(durations))
+
+    plt.plot(x, y)
+    plt.show()
 
 
 if __name__ == '__main__':
-    complete_graph_example(50)
+    run_tests()
+    # print(complete_graph_example(1000, 0, 1))
